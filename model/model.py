a45188ca49db30c3b2770cacf553051e8359c834
xiaohuanemiya <emiya172625@gmail.com>
2026-06-19 08:38:55 +0800

    add RMSNorm class and example usage in rmsnorm.py

--- model/model.py
+++ model/model.py
@@ -70,3 +70,20 @@ class EmiyaMindConfig(PretrainedConfig):
             if self.inference_rope_scaling
             else None
         )
+
+import torch
+import torch.nn as nn
+# 继承nn.Module类
+class RMSNorm(nn.Module):
+    # __init__初始化
+    def __int__(self,dim:int,eps:float=1e-5):
+        super().__init__()
+        self.dim=dim
+        self.eps=eps
+        self.weight=nn.Parameter(torch.ones(dim))
+    # _norm
+    def _norm(self,x):
+        return torch.rsqrt((x.pow(2).mean(-1,keepdim=True)+self.eps))*x
+    # forward
+    def forward(self,x):
+        return self.weight*self._norm(x.float()).type_as(x)
